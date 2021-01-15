048e42a7457e0a6b259d53b94bac87f874627294
David Entwistle <s2005971@baltic10.geos.ed.ac.uk>
2021-01-15 09:46:15 +0000

    Completed First Warm Up Task

---
+++ T01.py
@@ -0,0 +1,7 @@
+def NumbersCount(n):
+  # the loop
+  for i in range(1,n+1):   # a for loop loops from the first number to
+    print(i)               # the second number-1.
+                           # so to loop over 1-n inclusive, we must loop
+                           # to n+1. This is to make array indexing easier
+                           # more on this next
